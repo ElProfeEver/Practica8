e87c84a4e5782e7edc5f6f34723d7c3891cdd2c6
ElProfeEver <ever.unzueta@chih.conalep.edu.mx>
2025-05-08 08:21:35 -0600

    Edicion Script

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-function sumar() {
-    var num1;
-    num1 = document.calc.txtnumero1.value;
-    var num2;
-    num2 = document.calc.txtnumero2.value;
-    var opc;
-    opc = eval(num1) + eval(num2);
-    document.calc.resultado.value = opc;
+function cuenta(tipo){
+    var numero1;
+    numero1 = document.calc.txtnumero1.value;
+    var numero2;
+    numero2 = document.calc.txtnumero2.value;
+    if(tipo == "Suma") {
+    var result;
+    result = eval(numero1) + eval(numero2);
+    document.calc.resultado.value = result;
+    }
+    if(tipo == "Resta"){
+    var result;
+    result = eval(numero1) - eval(numero2);
+    document.calc.resultado.value = result;
+    }
+    if(tipo == "Division"){
+    var result;
+    result = eval(numero1) / eval(numero2);
+    document.calc.resultado.value = result;
+    }
+    if(tipo == "Multiplicacion"){
+    var result;
+    result = eval(numero1) * eval(numero2);
+    document.calc.resultado.value = result;
+    }
     }
